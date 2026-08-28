dda31a5428672cf632ea4dc2e75d287d425ea8ee
azuhata9876543-ops <azuhata9876543@gmail.com>
2026-08-28 10:39:51 +0900

    URLの変数を変更した。

--- frontend/moz-todo-react/src/Login.jsx
+++ frontend/moz-todo-react/src/Login.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
+const BASE_URL = import.meta.env.REACT_APP_API_URL || "http://localhost:8080";
 const API_URL = `${BASE_URL}/api/auth/login`;
 
 function Login() {
